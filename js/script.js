f4f760fbfcd468e6e81e1b810ebeeeef2d659afe
Daniel <daniel.tas@hotmail.com>
2019-03-07 16:59:38 -0300

    Alteracoes uteis

--- js/script.js
+++ js/script.js
@@ -1,48 +1,30 @@
-var qtd = 1;
-
 $(document).ready(function () {
-    $('form').submit(false);
+    $('#form').submit(false);
+    var i = 1;
 
-    $('#btn_add').click(function () {
-        qtd++;
-        $('#inputs').append('<div><input type="text" name="key' + qtd + '" class="form-control"><input type="text" name="value' + qtd + '" class="form-control"></div>');
+    $('#add').click(function() {
+        i++;
+        $('#campos_dinamicos').append('<tr id="linha'+i+'"><td><input type="text" class="form-control" id="key'+i+'" placeholder="Parâmetro"></td><td><input type="text" class="form-control" id="value'+i+'" placeholder="Valor"></td><td><button class="btn btn-danger remove">X</button></td></tr>');
     });
 
-    $('#btn_call').click(function () {
+    $(document).on('click', '.remove', function() {
+        var id_botao = $(this).attr('id');
+        $('#linha'+id_botao).remove();
+    });
+
+    $('#form').submit(function() {
         var urlInput = $('#url').val();
-        //var form = $(this);
         var tipo = $('#metodo').children("option:selected").val();
-        var texto;
+        var form = $(this);
+       
         $.ajax({
             url: "http://" + urlInput.replace("http://", ""),
-            //data: '',//form.serialize(),
             type: tipo,
+            data: form.serialize(),
             dataType: "json",
-            success: function (resp) {
-                // if (resp.code == 200) {
-                //     for (i in resp.dados) {
-                //         texto += resp.dados[i];
-                //         if (typeof(resp.dados[i]) == Object) {
-                //             for (j in resp.dados[i]) {
-                //                 texto += resp.dados[i][j];
-                //                 if (typeof(resp.dados[i][j]) == Object) {
-                //                     for (k in resp.dados[i][j]) {
-                //                         texto += resp.dados[i][j][k];
-                //                     }
-                //                 }
-                //             }
-                //         }
-                //     }
-                // } else {
-                //     for (i in resp.erros) {
-                //         for (j in resp.erros[i]) {
-                //             texto += resp.erros[i][j];
-                //         }
-                //     }
-                // }
-                
-                $('#resultado').html("<pre><code>" + JSON.stringify(resp, null, 4) + "</code></pre>");
-                console.log(resp);
+            success: function (resposta) {            
+                $('#resultado').html('<pre><code>' + JSON.stringify(resposta, null, 4) + '</code></pre>');
+                // console.log(resposta);
             },
             error: function (e) {
                 alert('Error: ' + e);
